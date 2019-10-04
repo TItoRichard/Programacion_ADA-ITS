7560224a4a72ddb6705bb736a9458c3bb62a2c1d
TItoRichard <56163245+TItoRichard@users.noreply.github.com>
2019-10-04 18:10:54 +0200

    Ejercicio Test4

---
+++ Primer Trimestre/test4.java
@@ -0,0 +1,22 @@
+public class test4 {
+    public static void main (String[] args) {
+        byte b = Byte.MAX_VALUE;
+        short s = Short.MAX_VALUE;
+        int i = Integer.MAX_VALUE;
+        long l = Long.MAX_VALUE;
+        float f = Float.MAX_VALUE;
+        double d = Double.MAX_VALUE;
+
+        int hex = 0x1a;
+        int bin = 0b0111;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(hex);
+        System.out.println(bin);
+    }
+}

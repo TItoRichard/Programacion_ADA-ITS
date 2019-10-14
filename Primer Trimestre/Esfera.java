3653d91dae600f6180ff56da1db19323fbfeab3a
TItoRichard <56163245+TItoRichard@users.noreply.github.com>
2019-10-14 17:55:09 +0200

    Esfera.java

---
+++ Primer Trimestre/Esfera.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//Area = 4 * pi * r^2
+//Volumen = 4/3 * pi * r^3
+//Radio = Diametro / 2
+
+public class Esfera {
+    public static void main (String [] args) {
+        System.out.print("Introduce un diametro de esfera: ");
+        Scanner diametro = new Scanner (System.in);
+        Int r = diametro;
+        Int radio = r/2;
+        System.out.println(radio);
+    }
+}
